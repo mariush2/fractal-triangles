cd46c4f34ada9f331ed35d0a2e4e1a4477b7b4c8
mariush2 <marius@continyou.no>
2017-10-07 16:32:28 +0200

    Added fractals going to the left, have to figure out a way to make them go right and top.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-var tri_width = 100
-var tri_height = -88
+var tri_width = 200 * 2
+var tri_height = -176 * 2
 
 function setup() {
   createCanvas(windowWidth / 1.2, windowHeight / 1.2);
@@ -7,27 +7,38 @@ function setup() {
 
 function draw() {
   background(51);
-  translate((width / 2) - tri_width * 1.5, (height / 2) - (tri_height / 4));
-  for (var i = 0; i < 3; i++) {
-    fract_rect(0, 0, tri_width / 2, tri_height, tri_width, 0);
-    translate(tri_width, 0);
-  }
+  translate(width / 2 - tri_width / 2, height / 1.3);
+  fract_rect(0, 0, tri_width / 2, tri_height, tri_width, 0, 0);
 }
 
-function fract_rect(x1, y1, x2, y2, x3, y3) {
+function fract_rect(x1, y1, x2, y2, x3, y3, n) {
   stroke(255);
   noFill();
-  //triangle(x1, y1, x2, y2, x3, y3);
-  y1 /= 2;
-  y2 /= 2;
-  y3 /= 2;
-  x1 /= 2;
+  triangle(x1, y1, x2, y2, x3, y3);
   x2 /= 2;
+  y2 /= 2;
   x3 /= 2;
-  //Draw two bot triangles
-  for (var i = 0; i < 2; i++) {
-    triangle(x1 + i * tri_width / 2, y1, x2 + i * tri_width / 2, y2, x3 + i * tri_width / 2, y3);
+  push();
+  translate(x2, y2)
+  triangle(x1, y1, x2, -y2, x3, y3);
+  pop();
+  if (n >= 0) {
+    n--;
+    //Left
+    fract_rect(x1, y1, x2, y2, x3, y3, n);
+
+    //Right
+    pop();
+    push();
+    translate(x2 * 2, 0);
+    fract_rect(x1, y1, x2, y2, x3, y3, n);
+    pop();
+
+    //Top
+    push();
+    translate(x2, y2);
+    fract_rect(x1, y1, x2, y2, x3, y3, n);
+    pop();
+
   }
-  // draw top inside triangle
-  triangle(x1 + tri_width / 4, y1 + tri_height / 2, x2 + tri_width / 4, y2 * 2, x3 + tri_width / 4, y3 + tri_height / 2);
 }
